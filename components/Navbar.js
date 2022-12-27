3e5f31022b14959d082bf48df224547a0a16a4a7
0xSunil <sunday7637@gmail.com>
2022-12-28 00:46:01 +0530

    solved some bugs

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,13 +2,24 @@ import { close, menu } from "../public/assets";
 import { MdOutlineMusicOff } from "react-icons/md";
 import { MdOutlineMusicNote } from "react-icons/md";
 import { navLinks } from "../Data";
-
+// import April from "../public/assets/April.wav";
 import Image from "next/image";
 import { use, useState } from "react";
 
 const NavBar = () => {
   const [tottle, setTottle] = useState(false);
-  const [music, setMusic] = useState(false);
+  const [music, setMusic] = useState(true);
+
+  // const play = () => {
+  //   new Audio(April).play();
+  // };
+
+  const playMusic = () => {
+    setMusic((tog) => {
+      return !tog;
+    });
+  };
+
   return (
     <nav className="w-full flex py-6 sm:py-8 sm:px-8 px-4 justify-between items-center navbar text-black bg">
       <h2 className="md:text-[50px] text-2xl text-black font-bold  font-rubik2 ">
@@ -18,13 +29,9 @@ const NavBar = () => {
       <div className="flex flex-row justify-center items-center z-10">
         <button
           className="object-contain hidden sm:flex text-black text-3xl px-10"
-          onClick={() => {
-            setMusic((tog) => {
-              return !tog;
-            });
-          }}
+          onClick={playMusic}
         >
-          {music ? <MdOutlineMusicOff /> : <MdOutlineMusicNote />}
+          {music ? <MdOutlineMusicNote /> : <MdOutlineMusicOff />}
         </button>
         <ul className="list-none hidden sm:flex justify-end items-center flex-1">
           {navLinks.map((nav, index) => (
@@ -42,11 +49,7 @@ const NavBar = () => {
       <div className="sm:hidden flex flex-1 justify-end items-center text-black">
         <button
           className="object-contain sm:hidden text-black text-3xl px-5"
-          onClick={() => {
-            setMusic((tog) => {
-              return !tog;
-            });
-          }}
+          onClick={playMusic}
         >
           {music ? <MdOutlineMusicOff /> : <MdOutlineMusicNote />}
         </button>
@@ -64,7 +67,7 @@ const NavBar = () => {
         <div
           className={`${
             tottle ? "flex" : "hidden"
-          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar text-black`}
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar text-black z-10`}
         >
           <ul className="list-none flex p-4 rounded-3xl  justify-end flex-col items-center flex-1 bg-pink-500 text-black  border-black">
             {navLinks.map((nav, index) => (
