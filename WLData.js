62b068e0e9a66e13f613a01bb7413d6e441d57e9
0xSunil <sunday7637@gmail.com>
2023-01-02 16:46:28 +0530

    fixed styles

--- WLData.js
+++ WLData.js
@@ -1,14 +1,10 @@
 const wl = [
   {
-    address: "0x6b2ea597d125cffa6c7b671cc36d9a63052e5350",
+    address: "0x6b2Ea597D125CfFa6C7b671CC36d9a63052e5350",
   },
   {
     address: "0x4dD4B5D6F696C5367444C0cCe7680DaaE8984a34",
   },
-  {
-    address: "0x4dD4B5D6F696C5367444C0cCe7680DaaE8984a34",
-  },
-
 ];
 
 // export function getFilteredAddress(address) {
